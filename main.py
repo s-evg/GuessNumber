7a3793a1ba17ebe2eb5f141147eebe325fb63759
s-evg <s-evg@bk.ru>
2021-12-05 16:27:00 +0500

    Исправление ошибок

--- main.py
+++ main.py
@@ -76,7 +76,7 @@ def GuessNumber(message):
         break
 
 
-@server.route('/' + TOKEN, method=['POST'])
+@server.route('/' + TOKEN, methods=['POST'])
 def get_message():
     json_string = request.get_data().decode('utf-8')
     update = telebot.types.Update.de_json(json_string)
